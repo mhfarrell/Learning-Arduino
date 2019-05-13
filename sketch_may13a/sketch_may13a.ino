feea55f58c73e079b9f039ddbf5f2700c9876aaa
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2019-05-13 19:54:52 +0100

    Create sketch_may13a.ino
    first project blink led

---
+++ sketch_may13a/sketch_may13a.ino
@@ -0,0 +1,14 @@
+int led = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second  
+}
